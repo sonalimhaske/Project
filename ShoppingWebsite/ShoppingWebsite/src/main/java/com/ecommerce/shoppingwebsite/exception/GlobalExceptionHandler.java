defd3f5f4e81bf55a817571ce375d1b98f50b5c2
sonalimhaske <96560519+sonalimhaske@users.noreply.github.com>
2022-03-06 11:56:49 +0530

    commit

--- ShoppingWebsite/ShoppingWebsite/src/main/java/com/ecommerce/shoppingwebsite/exception/GlobalExceptionHandler.java
+++ ShoppingWebsite/ShoppingWebsite/src/main/java/com/ecommerce/shoppingwebsite/exception/GlobalExceptionHandler.java
@@ -1,5 +1,30 @@
 package com.ecommerce.shoppingwebsite.exception;
 
-public class GlobalExceptionHandler {
 
-}
+
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+	public class  GlobalExceptionHandler {
+
+		@ExceptionHandler(ProductNotFoundException.class)
+	    public ResponseEntity<?> resourceNotFoundException(ProductNotFoundException ex, WebRequest request) {
+	         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+	         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+	    }
+
+	    @ExceptionHandler(Exception.class)
+	    public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
+	        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+	        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	}
+
+
--- ShoppingWebsite/ShoppingWebsite/src/main/java/com/ecommerce/shoppingwebsite/exception/ProductNotFoundException.java
+++ ShoppingWebsite/ShoppingWebsite/src/main/java/com/ecommerce/shoppingwebsite/exception/ProductNotFoundException.java
@@ -1,5 +1,16 @@
 package com.ecommerce.shoppingwebsite.exception;
 
-public class ProductNotFoundException {
 
-}
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+	public class  ProductNotFoundException extends Exception{
+
+	    private static final long serialVersionUID = 1L;
+
+	    public ProductNotFoundException(String message){
+	        super(message);
+	    }
+	}
+
